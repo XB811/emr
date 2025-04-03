9a12a13a77b68e05d9c2fa3fd5cf42c89bcf0422
xb <2098780588@qq.com>
2025-04-03 23:50:41 +0800

    feat:user 封装用户jwt校验解析等

---
+++ frameworks/bizs/user/src/main/java/top/xblog1/emr/framework/starter/user/config/UserAutoConfiguration.java
@@ -0,0 +1,26 @@
+package top.xblog1.emr.framework.starter.user.config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import top.xblog1.emr.framework.starter.user.core.UserTransmitFilter;
+
+import static top.xblog1.emr.framework.starter.bases.constant.FilterOrderConstant.USER_TRANSMIT_FILTER_ORDER;
+
+/**
+ * 用户配置自动装配
+ */
+@ConditionalOnWebApplication
+public class UserAutoConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(USER_TRANSMIT_FILTER_ORDER);
+        return registration;
+    }
+}
