8fac1a53fd7f925668c9017d4f11b475c62b9b60
xb <2098780588@qq.com>
2025-04-29 21:44:24 +0800

    fix: emr-user-services 新增token相关异常码

---
+++ services/user-services/src/main/java/top/xblog1/emr/services/user/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,35 @@
+package top.xblog1.emr.services.user.common.enums;
+
+
+import lombok.AllArgsConstructor;
+import top.xblog1.emr.framework.starter.convention.errorcode.IErrorCode;
+
+/**
+ *
+ */
+@AllArgsConstructor
+public enum UserErrorCodeEnum implements IErrorCode {
+    ILLEGAL_TOKE("A100001","非法token"),
+    OTHER_CLIENTS_LOGGED_IN("A100002","其他客户端登录"),
+    TOKEN_EXPIRED("A100003","Token过期"),
+    ;
+    /**
+     * 错误码
+     */
+    private final String code;
+
+    /**
+     * 错误提示消息
+     */
+    private final String message;
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
